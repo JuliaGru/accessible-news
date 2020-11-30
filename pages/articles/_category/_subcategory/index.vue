0ac7668ce44f08cd045cfca26824f015fbe4271e
JuliaGruber <ju.gruber@gmx.at>
2020-11-30 08:13:33 +0100

    make routing more independent

--- pages/articles/_category/_subcategory/index.vue
+++ pages/articles/_category/_subcategory/index.vue
@@ -15,16 +15,14 @@ export default {
   },
   asyncData (context) {
     return context.app.$storyapi.get('cdn/stories', {
-      starts_with: 'articles/international/eu/',
+      starts_with: 'articles/' + context.params.category + '/' + context.params.subcategory + '/',
       version: 'draft'
     }).then((res) => {
       return res.data
     }).catch((res) => {
       if (!res.response) {
-        console.error(res)
         context.error({ statusCode: 404, message: 'Failed to receive content form api' })
       } else {
-        console.error(res.response.data)
         context.error({ statusCode: res.response.status, message: res.response.data })
       }
     })
