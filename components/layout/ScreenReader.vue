3bf97764e33224c46b0dec920f4dcd3621d0c32a
JuliaGruber <ju.gruber@gmx.at>
2020-12-18 18:49:41 +0100

    color check

--- components/layout/ScreenReader.vue
+++ components/layout/ScreenReader.vue
@@ -2,8 +2,8 @@
   <section v-if="$parent.firstCall" class="fixed top-auto bottom-0 left-0 shadow w-full bg-white border-t p-4 text-center z-10">
     <p class="pb-2">Verwenden Sie einen Screen Reader?</p>
     <div class="flex justify-center">
-      <div role="button" @click="changeScreenReader(true)" class="bg-teal-600 text-white px-2 pb-1 mr-2 w-16">Ja</div>
-      <div role="button" @click="changeScreenReader(false)" class="bg-teal-600 text-white px-2 pb-1 w-16">Nein</div>
+      <div role="button" @click="changeScreenReader(true)" class="button font-bold px-2 py-1 mr-2 w-16">Ja</div>
+      <div role="button" @click="changeScreenReader(false)" class="button font-bold px-2 py-1 w-16">Nein</div>
     </div>
   </section>
 </template>
