571899a755a70bf9dddeb8cf1100797fe23d32fe
JuliaGruber <ju.gruber@gmx.at>
2020-12-20 20:51:31 +0100

    404 page

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :class="$store.state.store.theme">
+    <h1 v-if="error.statusCode === 404">Diese Seite wurde nicht gefunden.</h1>
+    <h1 v-else>Ein Fehler ist aufgetreten.</h1>
+    <NuxtLink to="/">Zurück zur Startseite</NuxtLink>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['error'],
+  layout: 'error'
+}
+</script>
+
