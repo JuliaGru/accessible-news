6c52a69c0156de28c82e06ca36999e9e261b15f0
JuliaGruber <ju.gruber@gmx.at>
2020-12-20 21:47:56 +0100

    purge test

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,16 +10,4 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography')
   ],
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
-    ],
-    whitelistPatterns: [/^theme-/],
-  }
 }
