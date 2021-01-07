45c49de6c93d23f9e0fc08c72b5ba39cd66a91e3
JuliaGruber <ju.gruber@gmx.at>
2021-01-07 17:23:43 +0100

    dup

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,8 @@ module.exports = {
   ],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: true,
+    enabled: false,
     content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
     ],
-    whitelist: ['.theme-interational'],
   }
 }
