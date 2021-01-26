0670961fb225bbe106c596247dfdd9c74a157691
JuliaGruber <ju.gruber@gmx.at>
2021-01-26 15:20:42 +0100

    subnavigation fixed height

--- components/layout/SubNavigation.vue
+++ components/layout/SubNavigation.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav id="subNavigation" v-if="nav.story" class="bg-var-light px-4 py-2 mb-4">
-    <ul class="flex">
+  <nav id="subNavigation" class="bg-var-light px-4 py-2 mb-4 h-12 flex items-center">
+    <ul class="flex" v-if="nav.story">
       <li v-for="nav_item in nav.story.content.navigation">
         <nuxt-link :to="'/articles/' + $route.params.category + nav_item.link.url">{{ nav_item.name }}</nuxt-link>
       </li>
