906cf19185dcd7da84b835e7dcb30c2dbf53187a
JuliaGru <ju.gruber@gmx.at>
2021-05-22 10:34:13 +0200

    change skiplinks

--- components/layout/SkipLinks.vue
+++ components/layout/SkipLinks.vue
@@ -1,17 +1,7 @@
 <template>
   <div>
-    <a href="#skiplink_navigation" class="skiplink focus:opacity-100 " :style="top">Zur Navigation</a>
-    <a href="#skiplink_content" class="skiplink focus:opacity-100 " :style="top">Zum Inhalt</a>
-    <a href="#skiplink_footer" class="skiplink focus:opacity-100 " :style="top">Zum Footer</a>
+    <a href="#skiplink_navigation" class="skiplink focus:opacity-100">Zur Navigation</a>
+    <a href="#skiplink_content" class="skiplink focus:opacity-100">Zum Inhalt</a>
+    <a href="#skiplink_footer" class="skiplink focus:opacity-100">Zum Footer</a>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      top: 'top: 5.5rem'
-    }
-  },
-}
-</script>
